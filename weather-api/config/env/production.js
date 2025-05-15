f7eca7e6ce9b62f9529fd08f4979381d5de08b50
Jake Unsted <jake.unsted@gmail.com>
2025-05-15 12:04:05 +0100

    production config

--- weather-api/config/env/production.js
+++ weather-api/config/env/production.js
@@ -148,9 +148,9 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     cors: {
-      // allowOrigins: [
-      //   'https://example.com',
-      // ]
+      allowOrigins: [
+        'https://ppfstest-production.up.railway.app'
+      ]
     },
 
   },
@@ -236,7 +236,7 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     cookie: {
-      // secure: true,
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000,  // 24 hours
     },
 
@@ -265,10 +265,9 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      'https://ppfstest-production.up.railway.app'
+    ],
 
 
     /***************************************************************************
@@ -340,7 +339,7 @@ module.exports = {
     * (https://sailsjs.com/config/http)                                        *
     *                                                                          *
     ***************************************************************************/
-    // trustProxy: true,
+    trustProxy: true,
 
   },
 
